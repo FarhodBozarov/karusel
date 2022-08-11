bae5487403bde798c995574a88256428ca1f23be
Farhod <farhodbozarov19752210@gmail.com>
2022-08-11 18:12:39 +0500

    uzgarish

--- karusel.js
+++ karusel.js
@@ -9,7 +9,7 @@ const massiv = [];
 let counter = 0;
 
 container.style.width = slidesToView * slideSize + "px";
-async function getImages(perPage = 20, page = 1) {
+async function getImages(perPage = 100, page = 3) {
   const result = await fetch(
     `https://api.pexels.com/v1/curated?per_page=${perPage}&page=${page}`,
     {
@@ -68,58 +68,3 @@ right.onclick = () => {
     container.style.transform = `translateX(${-counter * slideSize}px)`;
   }
 };
-
-// const API_KEY = "563492ad6f91700001000001655b3dcd6ee44b8c9bb3116140e09fc1";
-// const allImages = [];
-// const container = document.getElementById("container");
-// const left = document.getElementById("left");
-// const right = document.getElementById("right");
-// const slidesToView = 3;
-// const slideSize = 400;
-// let counter = 0;
-
-// container.style.width =
-//   slidesToView * slideSize + (slidesToView - 1) * 10 + "px";
-// async function getImages(perPage = 33, page = 2) {
-//   const result = await fetch(
-//     `https://api.pexels.com/v1/curated?per_page=${perPage}&page=${page}`,
-//     {
-//       method: "GET",
-//       headers: {
-//         accept: "application/json",
-//         Authorization: API_KEY,
-//       },
-//     }
-//   );
-//   const images = await result.json();
-//   images.photos.forEach((image) => {
-//     allImages.push(image.src.landscape);
-//   });
-//   render();
-// }
-
-// function render() {
-//   allImages.map((url) => {
-//     let img = document.createElement("img");
-//     img.src = url;
-//     img.width = slideSize;
-//     img.height = slideSize;
-//     img.style.objectFit = "cover";
-//     container.appendChild(img);
-//   });
-// }
-// getImages();
-
-// left.onclick = () => {
-//   if (counter < 0) {
-//     counter += 1;
-//     container.style.transform = `translateX(${counter * (slideSize + 10)}px)`;
-//   }
-// };
-
-// right.onclick = () => {
-//   if (counter > -allImages.length + 3) {
-//     counter -= 1;
-//     container.style.transform = `translateX(${counter * (slideSize + 10)}px)`;
-//   }
-// };
